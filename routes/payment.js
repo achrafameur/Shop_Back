26a4fc720f329b17c0c0407ddc76f5827167ed7a
achrafameur <achref.ameur1925@gmail.com>
2024-02-02 15:35:08 +0100

    ajout de standarsjs

--- routes/payment.js
+++ routes/payment.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const router = express.Router();
+const router = express.Router()
 
 const {
-    processPayment,
-    sendStripApi
+  processPayment,
+  sendStripApi
 } = require('../controllers/paymentController')
 
 const { isAuthenticatedUser } = require('../middlewares/auth')
 
-router.route('/payment/process').post(isAuthenticatedUser, processPayment);
-router.route('/stripeapi').get(isAuthenticatedUser, sendStripApi);
+router.route('/payment/process').post(isAuthenticatedUser, processPayment)
+router.route('/stripeapi').get(isAuthenticatedUser, sendStripApi)
 
-module.exports = router;
+module.exports = router
